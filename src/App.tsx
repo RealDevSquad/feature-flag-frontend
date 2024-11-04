4a0a9ad6db19df3e24271e3d91b6b559f1761cf7
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2024-11-05 00:02:12 +0530

    add: testing and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
-      <h1>
-        Real Dev Sqaud 
-      </h1>
+      <h1>Real Dev Sqaud</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
