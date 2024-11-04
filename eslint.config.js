841e0919f1ab2c9de5498fdad600852a9939007a
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2024-11-05 02:20:15 +0530

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,26 +3,79 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['dist/**'],
+  },
+  js.configs.recommended,
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      parser: tseslint.parser,
     },
     plugins: {
+      react: react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettier,
+      '@typescript-eslint': tseslint.plugin,
+      import: importPlugin,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
+      'no-use-before-define': 'off',
+      'no-shadow': 'off',
+      'no-unused-vars': 'off',
+      'no-underscore-dangle': 'off',
+      'import/extensions': 'off',
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      'import/prefer-default-export': 'off',
+      'import/no-unresolved': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+      'react/function-component-definition': [
+        'error',
+        { namedComponents: 'arrow-function' },
+      ],
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-use-before-define': ['error'],
+      '@typescript-eslint/no-shadow': ['error'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': ['error'],
     },
-  }
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.ts', '.tsx'],
+        },
+      },
+    },
+  },
 );
