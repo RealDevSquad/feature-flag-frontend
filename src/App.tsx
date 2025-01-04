521bc5002f5d4a137000c6fddac69ea2a3c44324
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-01-05 02:46:58 +0530

    nit: remove unwanted import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FeatureFlagList from './pages/FeatureFlagList';
 import FeatureFlagDetails from './pages/FeatureFlagDetails';
